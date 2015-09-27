26c08f2fa66b184b2857a8757403a3dd1592474d
legendarycat <legendary16823@gmail.com>
2015-09-27 19:35:49 +0800

    test

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
-
-$x=5;
-$y=10;
-
 function myTest() {
-    global $x,$y;
-    $y=$x+$y;
+   static $x=0;
+    echo $x;
+    $x++;
 }
 
 myTest();
-echo $y;
+echo"<br>";
+myTest();
+echo"<br>";
 ?>
