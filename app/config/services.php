10cb2b3c654a12bf3f6600a759ae32e0401ae82c
sdhou <xiaohou.don@gmail.com>
2015-10-02 11:50:45 +0800

    change .gitignore and config services

--- app/config/services.php
+++ app/config/services.php
@@ -1,14 +1,14 @@
 <?php
 
-use Phalcon\Mvc\View;
 use Phalcon\DI\FactoryDefault;
-use Phalcon\Mvc\Dispatcher;
-use Phalcon\Mvc\Url as UrlProvider;
-use Phalcon\Mvc\View\Engine\Volt as VoltEngine;
-use Phalcon\Mvc\Model\Metadata\Memory as MetaData;
-use Phalcon\Session\Adapter\Files as SessionAdapter;
-use Phalcon\Flash\Session as FlashSession;
 use Phalcon\Events\Manager as EventsManager;
+use Phalcon\Flash\Session as FlashSession;
+use Phalcon\Mvc\Dispatcher;
+use Phalcon\Mvc\Model\Metadata\Memory as MetaData;
+use Phalcon\Mvc\Url as UrlProvider;
+use Phalcon\Mvc\View;
+use Phalcon\Mvc\View\Engine\Volt as VoltEngine;
+use Phalcon\Session\Adapter\Files as SessionAdapter;
 
 /**
  * The FactoryDefault Dependency Injector automatically register the right services providing a full stack framework
@@ -20,45 +20,45 @@ $di = new FactoryDefault();
  */
 $di->set('dispatcher', function () use ($di) {
 
-	$eventsManager = new EventsManager;
+    $eventsManager = new EventsManager;
 
-	/**
-	 * Check if the user is allowed to access certain action using the SecurityPlugin
-	 */
-	$eventsManager->attach('dispatch:beforeDispatch', new SecurityPlugin);
+    /**
+     * Check if the user is allowed to access certain action using the SecurityPlugin
+     */
+    $eventsManager->attach('dispatch:beforeDispatch', new SecurityPlugin);
 
-	/**
-	 * Handle exceptions and not-found exceptions using NotFoundPlugin
-	 */
-	$eventsManager->attach('dispatch:beforeException', new NotFoundPlugin);
+    /**
+     * Handle exceptions and not-found exceptions using NotFoundPlugin
+     */
+    $eventsManager->attach('dispatch:beforeException', new NotFoundPlugin);
 
-	$dispatcher = new Dispatcher;
-	$dispatcher->setEventsManager($eventsManager);
+    $dispatcher = new Dispatcher;
+    $dispatcher->setEventsManager($eventsManager);
 
-	return $dispatcher;
+    return $dispatcher;
 });
 
 /**
  * The URL component is used to generate all kind of urls in the application
  */
 $di->set('url', function () use ($config) {
-	$url = new UrlProvider();
-	$url->setBaseUri($config->application->baseUri);
-	return $url;
+    $url = new UrlProvider();
+    $url->setBaseUri($config->application->baseUri);
+    return $url;
 });
 
 
 $di->set('view', function () use ($config) {
 
-	$view = new View();
+    $view = new View();
 
-	$view->setViewsDir(APP_PATH . $config->application->viewsDir);
+    $view->setViewsDir(APP_PATH . $config->application->viewsDir);
 
-	$view->registerEngines(array(
-		".volt" => 'volt'
-	));
+    $view->registerEngines([
+        ".volt" => 'volt',
+    ]);
 
-	return $view;
+    return $view;
 });
 
 /**
@@ -66,61 +66,61 @@ $di->set('view', function () use ($config) {
  */
 $di->set('volt', function ($view, $di) {
 
-	$volt = new VoltEngine($view, $di);
+    $volt = new VoltEngine($view, $di);
 
-	$volt->setOptions(array(
-		"compiledPath" => APP_PATH . "cache/volt/"
-	));
+    $volt->setOptions([
+        "compiledPath" => APP_PATH . "cache/volt/",
+    ]);
 
-	$compiler = $volt->getCompiler();
-	$compiler->addFunction('is_a', 'is_a');
+    $compiler = $volt->getCompiler();
+    $compiler->addFunction('is_a', 'is_a');
 
-	return $volt;
+    return $volt;
 }, true);
 
 /**
  * Database connection is created based in the parameters defined in the configuration file
  */
 $di->set('db', function () use ($config) {
-	$dbclass = 'Phalcon\Db\Adapter\Pdo\\' . $config->database->adapter;
-	return new $dbclass(array(
-		"host"     => $config->database->host,
-		"username" => $config->database->username,
-		"password" => $config->database->password,
-		"dbname"   => $config->database->name
-	));
+    $dbclass = 'Phalcon\Db\Adapter\Pdo\\' . $config->database->adapter;
+    return new $dbclass([
+        "host"     => $config->database->host,
+        "username" => $config->database->username,
+        "password" => $config->database->password,
+        "dbname"   => $config->database->name,
+    ]);
 });
 
 /**
  * If the configuration specify the use of metadata adapter use it or use memory otherwise
  */
 $di->set('modelsMetadata', function () {
-	return new MetaData();
+    return new MetaData();
 });
 
 /**
  * Start the session the first time some component request the session service
  */
 $di->set('session', function () {
-	$session = new SessionAdapter();
-	$session->start();
-	return $session;
+    $session = new SessionAdapter();
+    $session->start();
+    return $session;
 });
 
 /**
  * Register the flash service with custom CSS classes
  */
 $di->set('flash', function () {
-	return new FlashSession(array(
-		'error'   => 'alert alert-danger',
-		'success' => 'alert alert-success',
-		'notice'  => 'alert alert-info',
-	));
+    return new FlashSession([
+        'error'   => 'alert alert-danger',
+        'success' => 'alert alert-success',
+        'notice'  => 'alert alert-info',
+    ]);
 });
 
 /**
  * Register a user component
  */
 $di->set('elements', function () {
-	return new Elements();
+    return new Elements();
 });
